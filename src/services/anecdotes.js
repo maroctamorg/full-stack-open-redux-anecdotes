698770a2b316b739e02bad55af94e409aeb0edaf
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2026-02-18 15:09:56 +0200

    fix up backend URL for production deployment

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:3001/anecdotes'
 
 const getAll = async () => {
     const response = await axios.get(baseUrl)
